78d3915241ceefb62a0f2ab6ccadcdd0cab235c1
Sesman123 <pawel.suswal@gmail.com>
2022-10-21 11:26:18 +0200

    creation of 'ask_user_for_input' function

---
+++ app.py
@@ -0,0 +1,16 @@
+"""
+
+
+"""
+
+
+def ask_user_for_input(text_for_user):
+    """
+    Take user input
+    :param text_for_user: Message for user
+    :type text_for_user: str
+    :return: User input
+    :rtype: str
+    """
+
+    return input(text_for_user)
